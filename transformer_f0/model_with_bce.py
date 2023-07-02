953ee97364631489970beea14cf0e17a4fd658cd
YuriHead <ylzz1997@outlook.com>
2023-07-03 02:26:10 +0800

    debug

--- transformer_f0/model_with_bce.py
+++ transformer_f0/model_with_bce.py
@@ -8,9 +8,9 @@ from torchaudio.transforms import Resample
 from siren import Sine
 import numpy as np
 
-from .pcmer import PCmer
-from .pcmer_siren import PCmer as PCmerS
-from .nvSTFT import STFT
+from transformer_f0.pcmer import PCmer
+from transformer_f0.pcmer_siren import PCmer as PCmerS
+from transformer_f0.nvSTFT import STFT
 
 
 def l2_regularization(model, l2_alpha):
@@ -21,7 +21,7 @@ def l2_regularization(model, l2_alpha):
     return l2_alpha * sum(l2_loss)
 
 
-class TransformerF0(nn.Module):
+class TransformerF0BCE(nn.Module):
     def __init__(
             self,
             input_channel=128,
@@ -34,9 +34,9 @@ class TransformerF0(nn.Module):
             loss_l2_regularization_scale=1,
             loss_grad1_mse=False,
             loss_grad1_mse_scale=1,
-            f0_max = 1975.5,
-            f0_min = 32.70,
-            confidence = False
+            f0_max=1975.5,
+            f0_min=32.70,
+            confidence=False
     ):
         super().__init__()
         use_siren = False if (use_siren is None) else use_siren
@@ -46,11 +46,12 @@ class TransformerF0(nn.Module):
                                                                              is not None) else 1
         self.loss_grad1_mse = loss_grad1_mse if (loss_grad1_mse is not None) else False
         self.loss_grad1_mse_scale = loss_grad1_mse_scale if (loss_grad1_mse_scale is not None) else 1
-        self.f0_max = f0_max
-        self.f0_min = f0_min
-        
+        self.f0_max = f0_max if (f0_max is not None) else 1975.5
+        self.f0_min = f0_min if (f0_min is not None) else 32.70
+        self.confidence = confidence if (confidence is not None) else False
+
         self.cent_table = torch.Tensor(np.linspace(self.f0_to_cent(f0_min), self.f0_to_cent(f0_max), out_dims))
-        self.register_buffer("cent_table",self.cent_table)
+        self.register_buffer("cent_table", self.cent_table)
 
         # conv in stack
         _leaky = Sine(w0=1) if use_siren else nn.LeakyReLU()
@@ -85,7 +86,7 @@ class TransformerF0(nn.Module):
         self.n_out = out_dims
         self.dense_out = weight_norm(
             nn.Linear(n_chans, self.n_out))
-        
+
     def forward(self, mel, infer=True, gt_f0=None):
         """
         input:
@@ -96,12 +97,12 @@ class TransformerF0(nn.Module):
         x = self.stack(mel.transpose(1, 2)).transpose(1, 2)
         x = self.decoder(x)
         x = self.norm(x)
-        x = self.dense_out(x) #[B,N,D]
+        x = self.dense_out(x)  # [B,N,D]
         x = torch.sigmoid(x)
         if not infer:
             gt_cent_f0 = self.f0_to_cent(gt_f0)  # mel f0  #[B,N,1]
-            gt_cent_f0 = self.gaussian_blurred_cent(gt_cent_f0) # #[B,N,out_dim]
-            loss_all = self.loss_mse_scale * F.binary_cross_entropy(x,gt_cent_f0)  # bce loss
+            gt_cent_f0 = self.gaussian_blurred_cent(gt_cent_f0)  # #[B,N,out_dim]
+            loss_all = self.loss_mse_scale * F.binary_cross_entropy(x, gt_cent_f0)  # bce loss
             # l2 regularization
             if self.loss_l2_regularization:
                 loss_all = loss_all + l2_regularization(model=self, l2_alpha=self.loss_l2_regularization_scale)
@@ -111,11 +112,10 @@ class TransformerF0(nn.Module):
             x = self.cent_to_f0(x)
         return x
 
-    def cents_decoder(self,y):
-        B,N,_ = y.size()
-        ci = self.cent_table[None,None,:].expand(B,N,-1)
-        return torch.sum(ci*y,dim=-1,keepdim=True)/torch.sum(y,dim=-1,keepdim=True)
-    
+    def cents_decoder(self, y):
+        B, N, _ = y.size()
+        ci = self.cent_table[None, None, :].expand(B, N, -1)
+        return torch.sum(ci * y, dim=-1, keepdim=True) / torch.sum(y, dim=-1, keepdim=True)
 
     def cent_to_f0(self,cent):
         return 10.*torch.pow(2,cent/1200.)
@@ -123,11 +123,11 @@ class TransformerF0(nn.Module):
     def f0_to_cent(self,f0):
         return 1200.*torch.log2(f0/10.)
 
-    def gaussian_blurred_cent(self, cents): #cents: [B,N,1]
-        B,N,_ = cents.size()
-        ci = self.cent_table[None,None,:].expand(B,N,-1)
-        return torch.exp(-torch.square(ci-cents)/1250.)
-    
+    def gaussian_blurred_cent(self, cents):  # cents: [B,N,1]
+        B, N, _ = cents.size()
+        ci = self.cent_table[None, None, :].expand(B, N, -1)
+        return torch.exp(-torch.square(ci - cents) / 1250)
+
 
 class TransformerF0Infer:
     def __init__(self, model_path, device=None):
@@ -135,7 +135,7 @@ class TransformerF0Infer:
         with open(config_file, "r") as config:
             args = yaml.safe_load(config)
         self.args = DotDict(args)
-        model = TransformerF0(
+        model = TransformerF0BCE(
             input_channel=self.args.model.input_channel,
             out_dims=1,
             n_layers=self.args.model.n_layers,
