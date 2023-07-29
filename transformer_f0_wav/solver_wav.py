c3cd89e7734d1dfdb3c89c66e39fc6b5de108a52
ylzz1997 <ylzz1997@outlook.com>
2023-07-30 03:44:16 +0800

    分离代码

--- transformer_f0_wav/solver_wav.py
+++ transformer_f0_wav/solver_wav.py
@@ -8,7 +8,7 @@ from torch.cuda.amp import GradScaler
 
 from mir_eval.melody import raw_pitch_accuracy, to_cent_voicing, raw_chroma_accuracy, overall_accuracy
 from mir_eval.melody import voicing_recall, voicing_false_alarm
-
+import gc
 
 USE_MIR = True
 
@@ -131,104 +131,111 @@ def train(args, initial_global_step, model, optimizer, scheduler, loader_train, 
         raise ValueError(' [x] Unknown amp_dtype: ' + args.train.amp_dtype)
     for epoch in range(args.train.epochs):
         train_one_epoch(loader_train,saver,optimizer,model,dtype,scaler,epoch,args,scheduler,num_batches,loader_test)
+        # 手动gc,防止内存泄漏
+        gc.collect()
+
 
 def train_one_epoch(loader_train,saver,optimizer,model,dtype,scaler,epoch,args,scheduler,num_batches,loader_test):
     for batch_idx, data in enumerate(loader_train):
-        saver.global_step_increment()
-        optimizer.zero_grad()
+        train_one_step(batch_idx, data, saver,optimizer,model,dtype,scaler,epoch,args,scheduler,num_batches,loader_test)
 
-        # unpack data
-        for k in data.keys():
-            if not k.startswith('name'):
-                data[k] = data[k].to(args.device)
 
-        # forward
-        if dtype == torch.float32:
+def train_one_step(batch_idx, data, saver,optimizer,model,dtype,scaler,epoch,args,scheduler,num_batches,loader_test):
+    saver.global_step_increment()
+    optimizer.zero_grad()
+
+    # unpack data
+    for k in data.keys():
+        if not k.startswith('name'):
+            data[k] = data[k].to(args.device)
+
+    # forward
+    if dtype == torch.float32:
+        loss = model(mel=data['mel'], infer=False, gt_f0=data['f0'])
+    else:
+        with autocast(device_type=args.device, dtype=dtype):
             loss = model(mel=data['mel'], infer=False, gt_f0=data['f0'])
-        else:
-            with autocast(device_type=args.device, dtype=dtype):
-                loss = model(mel=data['mel'], infer=False, gt_f0=data['f0'])
 
-        # handle nan loss
-        if torch.isnan(loss):
-            # raise ValueError(' [x] nan loss ')
-            print(' [x] nan loss ')
-            loss = None
-            continue
+    # handle nan loss
+    if torch.isnan(loss):
+        # raise ValueError(' [x] nan loss ')
+        print(' [x] nan loss ')
+        loss = None
+        return
+    else:
+        # backpropagate
+        if dtype == torch.float32:
+            loss.backward()
+            optimizer.step()
         else:
-            # backpropagate
-            if dtype == torch.float32:
-                loss.backward()
-                optimizer.step()
-            else:
-                scaler.scale(loss).backward()
-                scaler.step(optimizer)
-                scaler.update()
-            scheduler.step()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+        scheduler.step()
+
+    # log loss
+    if saver.global_step % args.train.interval_log == 0:
+        current_lr = optimizer.param_groups[0]['lr']
+        saver.log_info(
+            'epoch: {} | {:3d}/{:3d} | {} | batch/s: {:.2f} | lr: {:.6} | loss: {:.3f} | time: {} | step: {}'.format(
+                epoch,
+                batch_idx,
+                num_batches,
+                args.env.expdir,
+                args.train.interval_log / saver.get_interval_time(),
+                current_lr,
+                loss.item(),
+                saver.get_total_time(),
+                saver.global_step
+            )
+        )
+
+        saver.log_value({
+            'train/loss': loss.item()
+        })
+
+        saver.log_value({
+            'train/lr': current_lr
+        })
+
+    # validation
+    if saver.global_step % args.train.interval_val == 0:
+        optimizer_save = optimizer if args.train.save_opt else None
+
+        # save latest
+        saver.save_model(model, optimizer_save, postfix=f'{saver.global_step}')
+        last_val_step = saver.global_step - args.train.interval_val
+        if last_val_step % args.train.interval_force_save != 0:
+            saver.delete_model(postfix=f'{last_val_step}')
+
+        # run testing set
+        test_loss, rpa, rca, oa, vfa, vr = test(args, model, loader_test, saver)
 
         # log loss
-        if saver.global_step % args.train.interval_log == 0:
-            current_lr = optimizer.param_groups[0]['lr']
-            saver.log_info(
-                'epoch: {} | {:3d}/{:3d} | {} | batch/s: {:.2f} | lr: {:.6} | loss: {:.3f} | time: {} | step: {}'.format(
-                    epoch,
-                    batch_idx,
-                    num_batches,
-                    args.env.expdir,
-                    args.train.interval_log / saver.get_interval_time(),
-                    current_lr,
-                    loss.item(),
-                    saver.get_total_time(),
-                    saver.global_step
-                )
+        saver.log_info(
+            ' --- <validation> --- \nloss: {:.3f}. '.format(
+                test_loss,
             )
+        )
 
+        saver.log_value({
+            'validation/loss': test_loss
+        })
+        if USE_MIR:
             saver.log_value({
-                'train/loss': loss.item()
+                'validation/rpa': rpa
+            })
+            saver.log_value({
+                'validation/rca': rca
+            })
+            saver.log_value({
+                'validation/oa': oa
+            })
+            saver.log_value({
+                'validation/vfa': vfa
+            })
+            saver.log_value({
+                'validation/vr': vr
             })
 
-            saver.log_value({
-                'train/lr': current_lr
-            })
-
-        # validation
-        if saver.global_step % args.train.interval_val == 0:
-            optimizer_save = optimizer if args.train.save_opt else None
-
-            # save latest
-            saver.save_model(model, optimizer_save, postfix=f'{saver.global_step}')
-            last_val_step = saver.global_step - args.train.interval_val
-            if last_val_step % args.train.interval_force_save != 0:
-                saver.delete_model(postfix=f'{last_val_step}')
-
-            # run testing set
-            test_loss, rpa, rca, oa, vfa, vr = test(args, model, loader_test, saver)
-
-            # log loss
-            saver.log_info(
-                ' --- <validation> --- \nloss: {:.3f}. '.format(
-                    test_loss,
-                )
-            )
-
-            saver.log_value({
-                'validation/loss': test_loss
-            })
-            if USE_MIR:
-                saver.log_value({
-                    'validation/rpa': rpa
-                })
-                saver.log_value({
-                    'validation/rca': rca
-                })
-                saver.log_value({
-                    'validation/oa': oa
-                })
-                saver.log_value({
-                    'validation/vfa': vfa
-                })
-                saver.log_value({
-                    'validation/vr': vr
-                })
-
-            model.train()
+        model.train()
