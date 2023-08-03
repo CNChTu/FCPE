de4ded561a51c4c2eb756a1e8890f7a3ebaf1d93
CN_ChiTu <36254426+CNChTu@users.noreply.github.com>
2023-08-03 19:33:52 +0800

    fix bug

--- transformer_f0_wav/data_loaders_wav.py
+++ transformer_f0_wav/data_loaders_wav.py
@@ -201,169 +201,175 @@ class F0Dataset(Dataset):
         else:
             print('Load the f0, volume data from :', path_root)
 
-        with ProcessPoolExecutor(max_workers=load_data_num_processes) as executor:
-            tasks = []
-            for i in range(load_data_num_processes):
-                start = int(i * len(self.paths) / load_data_num_processes)
-                end = int((i + 1) * len(self.paths) / load_data_num_processes)
-                file_chunk = self.paths[start:end]
-                tasks.append(file_chunk)
-            for data_buffer in executor.map(self.load_data, tasks):
-                self.data_buffer.update(data_buffer)
+        with torch.no_grad():
+            with ProcessPoolExecutor(max_workers=load_data_num_processes) as executor:
+                tasks = []
+                for i in range(load_data_num_processes):
+                    start = int(i * len(self.paths) / load_data_num_processes)
+                    end = int((i + 1) * len(self.paths) / load_data_num_processes)
+                    file_chunk = self.paths[start:end]
+                    tasks.append(file_chunk)
+                for data_buffer in executor.map(self.load_data, tasks):
+                    self.data_buffer.update(data_buffer)
 
 
-        self.paths = np.array(self.paths, dtype = object)
-        self.data_buffer = pd.DataFrame(self.data_buffer)
+            self.paths = np.array(self.paths, dtype = object)
+            self.data_buffer = pd.DataFrame(self.data_buffer)
 
     def load_data(self, paths):
-        data_buffer = {}
-        rank = mp.current_process()._identity
-        rank = rank[0] if len(rank) > 0 else 0
-        for name_ext in tqdm(paths):
-            path_audio = os.path.join(self.path_root, 'audio', name_ext)
-            duration = librosa.get_duration(filename=path_audio, sr=self.sample_rate)
+        with torch.no_grad():
+            data_buffer = {}
+            rank = mp.current_process()._identity
+            rank = rank[0] if len(rank) > 0 else 0
+            for name_ext in tqdm(paths):
+                path_audio = os.path.join(self.path_root, 'audio', name_ext)
+                duration = librosa.get_duration(filename=path_audio, sr=self.sample_rate)
 
-            path_f0 = os.path.join(self.path_root, 'f0', name_ext) + '.npy'
-            f0 = np.load(path_f0)[:, None]
-            # f0 = torch.from_numpy(f0).float().unsqueeze(-1).to(self.device)
+                path_f0 = os.path.join(self.path_root, 'f0', name_ext) + '.npy'
+                f0 = np.load(path_f0)[:, None]
+                # f0 = torch.from_numpy(f0).float().unsqueeze(-1).to(self.device)
 
-            if self.n_spk is not None and self.n_spk > 1:
-                dirname_split = re.split(r"_|\-", os.path.dirname(name_ext), 2)[0]
-                t_spk_id = spk_id = int(dirname_split) if str.isdigit(dirname_split) else 0
-                if spk_id < 1 or spk_id > self.n_spk:
-                    raise ValueError(
-                        ' [x] Muiti-speaker traing error : spk_id must be a positive integer from 1 to n_spk ')
-            else:
-                pass
-                #spk_id = 1
-                #t_spk_id = spk_id
-            # spk_id = torch.LongTensor(np.array([spk_id])).to(self.device)
-            #spk_id = np.array([spk_id])
+                if self.n_spk is not None and self.n_spk > 1:
+                    dirname_split = re.split(r"_|\-", os.path.dirname(name_ext), 2)[0]
+                    t_spk_id = spk_id = int(dirname_split) if str.isdigit(dirname_split) else 0
+                    if spk_id < 1 or spk_id > self.n_spk:
+                        raise ValueError(
+                            ' [x] Muiti-speaker traing error : spk_id must be a positive integer from 1 to n_spk ')
+                else:
+                    pass
+                    #spk_id = 1
+                    #t_spk_id = spk_id
+                # spk_id = torch.LongTensor(np.array([spk_id])).to(self.device)
+                #spk_id = np.array([spk_id])
 
-            if self.load_all_data:
-                '''
-                audio, sr = librosa.load(path_audio, sr=self.sample_rate)
-                if len(audio.shape) > 1:
-                    audio = librosa.to_mono(audio)
-                audio = torch.from_numpy(audio).to(device)
-                '''
-                path_audio = os.path.join(self.path_root, 'npaudiodir', name_ext) + '.npy'
-                audio = np.load(path_audio)
+                if self.load_all_data:
+                    '''
+                    audio, sr = librosa.load(path_audio, sr=self.sample_rate)
+                    if len(audio.shape) > 1:
+                        audio = librosa.to_mono(audio)
+                    audio = torch.from_numpy(audio).to(device)
+                    '''
+                    path_audio = os.path.join(self.path_root, 'npaudiodir', name_ext) + '.npy'
+                    audio = np.load(path_audio)
 
-                """
-                data_buffer[name_ext] = {
-                    'duration': duration,
-                    'audio': audio,
-                    'f0': f0,
-                    'spk_id': spk_id,
-                    't_spk_id': t_spk_id,
-                }
-                """
-                data_buffer[name_ext] = (duration, f0, audio)
-            else:
-                """
-                data_buffer[name_ext] = {
-                    'duration': duration,
-                    'f0': f0,
-                    'spk_id': spk_id,
-                    't_spk_id': t_spk_id
-                }
-                """
-                data_buffer[name_ext] = (duration, f0)
-        return data_buffer
+                    """
+                    data_buffer[name_ext] = {
+                        'duration': duration,
+                        'audio': audio,
+                        'f0': f0,
+                        'spk_id': spk_id,
+                        't_spk_id': t_spk_id,
+                    }
+                    """
+                    data_buffer[name_ext] = (duration, f0, audio)
+                else:
+                    """
+                    data_buffer[name_ext] = {
+                        'duration': duration,
+                        'f0': f0,
+                        'spk_id': spk_id,
+                        't_spk_id': t_spk_id
+                    }
+                    """
+                    data_buffer[name_ext] = (duration, f0)
+            return data_buffer
 
     def __getitem__(self, file_idx):
-        name_ext = self.paths[file_idx]
-        data_buffer = self.data_buffer[name_ext]
-        # check duration. if too short, then skip
-        if data_buffer[0] < (self.waveform_sec + 0.1):
-            return self.__getitem__((file_idx + 1) % len(self.paths))
+        with torch.no_grad():
+            name_ext = self.paths[file_idx]
+            data_buffer = self.data_buffer[name_ext]
+            # check duration. if too short, then skip
+            if data_buffer[0] < (self.waveform_sec + 0.1):
+                return self.__getitem__((file_idx + 1) % len(self.paths))
 
-        # get item
-        return self.get_data(name_ext, data_buffer)
+            # get item
+            return self.get_data(name_ext, data_buffer)
 
     def get_data(self, name_ext, data_buffer):
-        name = os.path.splitext(name_ext)[0]
-        frame_resolution = self.hop_size / self.sample_rate
-        duration = data_buffer[0]
-        waveform_sec = duration if self.whole_audio else self.waveform_sec
+        with torch.no_grad():
+            name = os.path.splitext(name_ext)[0]
+            frame_resolution = self.hop_size / self.sample_rate
+            duration = data_buffer[0]
+            waveform_sec = duration if self.whole_audio else self.waveform_sec
 
-        # load audio
-        idx_from = 0 if self.whole_audio else random.uniform(0, duration - waveform_sec - 0.1)
-        start_frame = int(idx_from / frame_resolution)
-        units_frame_len = int(waveform_sec / frame_resolution)
+            # load audio
+            idx_from = 0 if self.whole_audio else random.uniform(0, duration - waveform_sec - 0.1)
+            start_frame = int(idx_from / frame_resolution)
+            units_frame_len = int(waveform_sec / frame_resolution)
 
-        # load f0
-        f0 = data_buffer[1]
-        f0 = torch.from_numpy(f0).float().cpu()
+            # load f0
+            f0 = data_buffer[1].copy()
+            f0 = torch.from_numpy(f0).float().cpu()
 
-        # load mel
-        #audio = data_buffer.get('audio')
-        if len(data_buffer) == 2:
-            path_audio = os.path.join(self.path_root, 'npaudiodir', name_ext) + '.npy'
-            audio = np.load(path_audio)
-        else:
-            audio = data_buffer[2]
-        
-        if random.choice((False, True)) and self.aug_eq:
-            audio = ut.random_eq(audio, self.sample_rate)
-            audio = ut.random_eq(audio, self.sample_rate)
-
-        if random.choice((False, True)) and self.aug_eq:
-            audio = ut.random_eq(audio, self.sample_rate)
-
-        if random.choice((False, True)) and self.aug_eq:
-            audio = ut.random_eq(audio, self.sample_rate)
-
-        if random.random()<0.2 and self.aug_reverb:
-            audio = ru.加点混响(audio)
-
-        if random.choice((False, True)) and self.aug_keyshift:
-            keyshift = random.uniform(self.keyshift_min, self.keyshift_max)
-            f0 = 2 ** (keyshift / 12) * f0
-        else:
-            keyshift = 0
-        
-        is_aug_noise = bool(random.randint(0, 1))
-        if self.aug_noise and is_aug_noise:
-            if bool(random.randint(0, 1)):
-                audio = ut.add_noise(audio, noise_ratio=self.noise_ratio)
+            # load mel
+            #audio = data_buffer.get('audio')
+            if len(data_buffer) == 2:
+                path_audio = os.path.join(self.path_root, 'npaudiodir', name_ext) + '.npy'
+                audio = np.load(path_audio)
             else:
-                audio = ut.add_noise_slice(audio,self.sample_rate,self.duration,noise_ratio=self.noise_ratio, brown_noise_ratio=self.brown_noise_ratio)
-                
-        peak = np.abs(audio).max()
-        audio = 0.98 * audio / peak
-        audio = torch.from_numpy(audio).float().unsqueeze(0).cpu()
-        mel = self.wav2mel(audio, sample_rate=self.sample_rate, keyshift=keyshift, train=True).squeeze(0).cpu()
+                audio = data_buffer[2].copy()
 
-        if self.aug_mask and bool(random.randint(0, 1)) and not is_aug_noise:
-            v_o = bool(random.randint(0, 1)) and self.aug_mask_v_o
-            mel = mel.transpose(-1,-2)
-            if self.aug_mask_iszeropad_mode == 'zero':
-                iszeropad = True
-            elif self.aug_mask_iszeropad_mode =='noise':
-                iszeropad = False
+            if random.choice((False, True)) and self.aug_eq:
+                audio = ut.random_eq(audio, self.sample_rate)
+                audio = ut.random_eq(audio, self.sample_rate)
+
+            if random.choice((False, True)) and self.aug_eq:
+                audio = ut.random_eq(audio, self.sample_rate)
+
+            if random.choice((False, True)) and self.aug_eq:
+                audio = ut.random_eq(audio, self.sample_rate)
+
+            if random.random()<0.2 and self.aug_reverb:
+                audio = ru.加点混响(audio)
+
+            if random.choice((False, True)) and self.aug_keyshift:
+                keyshift = random.uniform(self.keyshift_min, self.keyshift_max)
+                with torch.no_grad():
+                    f0 = 2 ** (keyshift / 12) * f0
             else:
-                iszeropad = bool(random.randint(0, 1))
-            mel = ut.add_mel_mask_slice(mel,self.sample_rate,self.duration,hop_size=self.hop_size,vertical_factor=self.aug_mask_vertical_factor_v_o if v_o else self.aug_mask_vertical_factor,vertical_offset=v_o,iszeropad=iszeropad,block_num=self.aug_mask_block_num_v_o if v_o else self.aug_mask_block_num)
-            mel = mel.transpose(-1,-2)
-            
-        mel = mel[start_frame: start_frame + units_frame_len].detach()
+                keyshift = 0
 
-        f0_frames = f0[start_frame: start_frame + units_frame_len]
+            is_aug_noise = bool(random.randint(0, 1))
+            if self.aug_noise and is_aug_noise:
+                if bool(random.randint(0, 1)):
+                    audio = ut.add_noise(audio, noise_ratio=self.noise_ratio)
+                else:
+                    audio = ut.add_noise_slice(audio,self.sample_rate,self.duration,noise_ratio=self.noise_ratio, brown_noise_ratio=self.brown_noise_ratio)
 
-        # load spk_id
-        #spk_id = data_buffer.get('spk_id')
-        #spk_id = torch.LongTensor(spk_id).to(self.device)
+            peak = np.abs(audio).max()
+            audio = 0.98 * audio / peak
+            audio = torch.from_numpy(audio).float().unsqueeze(0).cpu()
+            with torch.no_grad():
+                mel = self.wav2mel(audio, sample_rate=self.sample_rate, keyshift=keyshift, train=True).squeeze(0).cpu()
 
-        if random.choice((False, False, True)) and self.aug_flip:
-            f0_frames = torch.flip(f0_frames, dims=[0])
-            mel = torch.flip(mel, dims=[0])
-        
-        del audio
-        #return dict(mel=mel, f0=f0_frames, spk_id=spk_id, name=name, name_ext=name_ext)
-        output = (mel, f0_frames, name, name_ext)
-        return output
+            if self.aug_mask and bool(random.randint(0, 1)) and not is_aug_noise:
+                v_o = bool(random.randint(0, 1)) and self.aug_mask_v_o
+                mel = mel.transpose(-1,-2)
+                if self.aug_mask_iszeropad_mode == 'zero':
+                    iszeropad = True
+                elif self.aug_mask_iszeropad_mode =='noise':
+                    iszeropad = False
+                else:
+                    iszeropad = bool(random.randint(0, 1))
+                mel = ut.add_mel_mask_slice(mel,self.sample_rate,self.duration,hop_size=self.hop_size,vertical_factor=self.aug_mask_vertical_factor_v_o if v_o else self.aug_mask_vertical_factor,vertical_offset=v_o,iszeropad=iszeropad,block_num=self.aug_mask_block_num_v_o if v_o else self.aug_mask_block_num)
+                mel = mel.transpose(-1,-2)
+
+            mel = mel[start_frame: start_frame + units_frame_len].detach()
+
+            f0_frames = f0[start_frame: start_frame + units_frame_len].detach()
+
+            # load spk_id
+            #spk_id = data_buffer.get('spk_id')
+            #spk_id = torch.LongTensor(spk_id).to(self.device)
+
+            if random.choice((False, False, True)) and self.aug_flip:
+                f0_frames = torch.flip(f0_frames, dims=[0])
+                mel = torch.flip(mel, dims=[0])
+
+            del audio
+            #return dict(mel=mel, f0=f0_frames, spk_id=spk_id, name=name, name_ext=name_ext)
+            output = (mel, f0_frames, name, name_ext)
+            return output
 
     def __len__(self):
         return len(self.paths)
