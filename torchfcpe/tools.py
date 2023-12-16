3e2bf2b8d93f77d16f104896f10da98eef3acdd8
CNChiTu <2921046558@qq.com>
2023-12-17 01:43:29 +0800

    'v0.0.2'

--- torchfcpe/tools.py
+++ torchfcpe/tools.py
@@ -352,19 +352,26 @@ def get_device(device: str, func_name: str) -> str:
     """Get device"""
 
     if device is None:
-        device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+        if torch.cuda.is_available():
+            device = 'cuda'
+        elif torch.backends.mps.is_available():
+            device = 'mps'
+        else:
+            device = 'cpu'
+
         print(f'  [INFO]: Using {device} automatically.')
         print(f'  [INFO]    > call by: {func_name}')
-
-    # Check if the specified device is available, if not, switch to cpu
-    if device == 'cuda' and not torch.cuda.is_available() or device == 'mps' and not torch.backends.mps.is_available():
-        print(f'  [WARNING]: Specified device ({device}) is not available, switching to cpu.')
-        device = 'cpu'
-    
     else:
         print(f'  [INFO]: device is not None, use {device}')
         print(f'  [INFO]    > call by:{func_name}')
         device = device
+
+    # Check if the specified device is available, if not, switch to cpu
+    if ((device == 'cuda' and not torch.cuda.is_available()) or
+            (device == 'mps' and not torch.backends.mps.is_available())):
+        print(f'  [WARN]: Specified device ({device}) is not available, switching to cpu.')
+        device = 'cpu'
+
     return device
 
 
